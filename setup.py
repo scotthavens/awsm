303f62575d254e90179837ef056ec9d6e81c316a
Micah Sandusky <micah.sandusky@ars.usda.gov>
2018-12-14 18:51:21 +0000

    starting script that can be called from airflow to run a weekly snowav report

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     scripts=['./scripts/awsm','./scripts/wyhr',
 			 './scripts/plot_csv', './scripts/plot_ipw',
 			 './scripts/clean_awsm', './scripts/awsm_daily',
-			 './scripts/awsm_daily_airflow'],
+			 './scripts/awsm_daily_airflow', './scripts/snowav_weekly_airflow'],
     # install_requires=requirements,
     license="CC0 1.0",
     zip_safe=False,
